ba5bbda0326f9ce2227565b756adc3aa42cbf66c
Guy Geva <guy1030@gmail.com>
2021-07-08 21:42:02 +0300

    first commit

---
+++ api/toy/toy-controller.js
@@ -0,0 +1,72 @@
+const toyService = require('./toy-service')
+const socketService = require('../../services/socket.service')
+const logger = require('../../services/logger.service')
+
+module.exports = {
+  getToy,
+  getToys,
+  deleteToy,
+  addToy,
+  updateToy
+}
+
+async function getToy(req, res) {
+  try {
+    const toy = await toyService.getById(req.params.id)
+    res.send(toy)
+  } catch (err) {
+    // logger.error('Failed to get toy', err)
+    res.status(500).send({ err: 'Failed to get toy' })
+  }
+}
+
+async function getToys(req, res) {
+  try {
+    // const filterBy = {
+    //   txt: req.query?.txt || '',
+    //   minBalance: +req.query?.minBalance || 0
+    // }
+    const filterBy = ''
+    const toys = await toyService.query(filterBy)
+    res.send(toys)
+  } catch (err) {
+    logger.error('Failed to get toys', err)
+    res.status(500).send({ err: 'Failed to get toys' })
+  }
+}
+
+async function deleteToy(req, res) {
+  try {
+    await toyService.remove(req.params.id)
+    res.send({ msg: 'Deleted successfully' })
+  } catch (err) {
+    logger.error('Failed to delete user', err)
+    res.status(500).send({ err: 'Failed to delete user' })
+  }
+}
+
+async function addToy(req, res) {
+  try {
+    const toy = req.body
+    const savedToy = await toyService.add(toy)
+    res.send(savedToy)
+    // socketService.broadcast({ type: 'user-updated', data: review, to: savedUser._id })
+  } catch (err) {
+    logger.error('Failed to add toy', err)
+    res.status(500).send({ err: 'Failed to add toy' })
+  }
+}
+
+async function updateToy(req, res) {
+  try {
+    const toy = req.body
+    console.log(toy);
+    const savedToy = await toyService.update(toy)
+    res.send(savedToy)
+    // socketService.broadcast({ type: 'user-updated', data: review, to: savedUser._id })
+  } catch (err) {
+    logger.error('Failed to update toy', err)
+    res.status(500).send({ err: 'Failed to update toy' })
+  }
+}
+
---
+++ api/toy/toy-service.js
@@ -0,0 +1,124 @@
+
+const dbService = require('../../services/db.service')
+// const logger = require('../../services/logger.service')
+// const reviewService = require('../review/review.service')
+const ObjectId = require('mongodb').ObjectId
+
+module.exports = {
+  query,
+  getById,
+  remove,
+  update,
+  add
+}
+
+async function query(filterBy = {}) {
+  const criteria = _buildCriteria(filterBy)
+  try {
+    const collection = await dbService.getCollection('toy')
+    var toys = await collection.find(criteria).toArray()
+    return toys
+  } catch (err) {
+    logger.error('cannot find toys', err)
+    throw err
+  }
+}
+
+async function getById(toyId) {
+  try {
+    const collection = await dbService.getCollection('toy')
+    const toy = await collection.findOne({ '_id': ObjectId(toyId) })
+    return toy
+  } catch (err) {
+    logger.error(`while finding toy ${toyId}`, err)
+    throw err
+  }
+}
+
+async function remove(toyId) {
+  try {
+    const collection = await dbService.getCollection('toy')
+    await collection.deleteOne({ '_id': ObjectId(toyId) })
+  } catch (err) {
+    logger.error(`cannot remove toy ${toyId}`, err)
+    throw err
+  }
+}
+
+async function add(toy) {
+  try {
+    const toyToAdd = {
+      name: toy.name,
+      price: toy.price,
+      type: toy.type,
+      createdAt: toy.createdAt,
+      inStock: toy.inStock
+    }
+    const collection = await dbService.getCollection('toy')
+    await collection.insertOne(toyToAdd)
+    return toyToAdd
+  } catch (err) {
+    logger.error('cannot insert toy', err)
+    throw err
+  }
+}
+
+async function update(toy) {
+  try {
+    // peek only updatable fields!
+    const toyToSave = {
+      _id: ObjectId(toy._id),
+      name: toy.name,
+      price: toy.price,
+      type: toy.type,
+      createdAt: toy.createdAt,
+      inStock: toy.inStock
+    }
+    const collection = await dbService.getCollection('toy')
+    await collection.updateOne({ '_id': toyToSave._id }, { $set: toyToSave })
+    return toyToSave;
+  } catch (err) {
+    logger.error(`cannot update toy ${toy._id}`, err)
+    throw err
+  }
+}
+
+async function add(toy) {
+  try {
+    // peek only updatable fields!
+    const toyToAdd = {
+      name: toy.toyname,
+      price: toy.fullname,
+      type: toy.type,
+      createdAt: toy.createdAt,
+      inStock: toy.inStock
+    }
+    const collection = await dbService.getCollection('toy')
+    await collection.insertOne(toyToAdd)
+    return toyToAdd
+  } catch (err) {
+    logger.error('cannot insert toy', err)
+    throw err
+  }
+}
+
+function _buildCriteria(filterBy) {
+  const criteria = {}
+  if (filterBy.txt) {
+    const txtCriteria = { $regex: filterBy.txt, $options: 'i' }
+    criteria.$or = [
+      {
+        toyname: txtCriteria
+      },
+      {
+        fullname: txtCriteria
+      }
+    ]
+  }
+  if (filterBy.minBalance) {
+    criteria.balance = { $gte: filterBy.minBalance }
+  }
+  return criteria
+}
+
+

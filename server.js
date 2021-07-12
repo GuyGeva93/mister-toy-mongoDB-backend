3804d4f9dce2f212d695060418574fef843cc5b3
Guy Geva <guy1030@gmail.com>
2021-07-12 21:55:57 +0300

    feat: simple chat with socket.io

--- server.js
+++ server.js
@@ -5,42 +5,51 @@ const expressSession = require('express-session')
 
 const app = express()
 const http = require('http').createServer(app)
+var io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
 
 const session = expressSession({
-    secret: 'coding is amazing',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
+  secret: 'coding is amazing',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
 })
 // Express App Config
 app.use(express.json())
 app.use(session)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')))
+  app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
-    const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
+  const corsOptions = {
+    origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+    credentials: true
+  }
+  app.use(cors(corsOptions))
 }
 
 const toyRoutes = require('./api/toy/toy-routes')
 const authRoutes = require('./api/auth/auth-routes')
 const userRoutes = require('./api/user/user-routes')
 const reviewRoutes = require('./api/review/review.routes')
-const {connectSockets} = require('./services/socket-service')
+const { connectSockets } = require('./services/socket-service')
 
 // routes
 const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
 app.all('*', setupAsyncLocalStorage)
 
 // TODO: check with app.use
-app.get('/api/setup-session', (req, res) =>{
-    req.session.connectedAt = Date.now()
-    console.log('setup-session:', req.sessionID);
-    res.end()
+app.get('/api/setup-session', (req, res) => {
+  req.session.connectedAt = Date.now()
+  console.log('setup-session:', req.sessionID);
+  res.end()
 })
 
 app.use('/api/toy', toyRoutes)
@@ -59,7 +68,7 @@ connectSockets(http, session)
 const logger = require('./services/logger-service')
 const port = process.env.PORT || 3030
 http.listen(port, () => {
-    logger.info('Server is running on port: ' + port)
+  logger.info('Server is running on port: ' + port)
 })
 
 console.log('I am Here!, am I?')
